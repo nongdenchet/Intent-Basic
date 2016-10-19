6dcea860ecf151f141089350768567683f81b644
Quan Vu <nongdenchet8435848@gmail.com>
2016-10-19 14:49:33 +0700

    update package

--- app/src/androidTest/java/apidez/com/intent/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/apidez/com/intent/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("apidez.com.week3", appContext.getPackageName());
+        assertEquals("apidez.com.intent", appContext.getPackageName());
     }
 }
